c26cfb3307cfd0a61b10df3cefee76fdecde2330
Bachynskyi Alex <balex70@yahoo.com>
2026-03-20 13:49:58 +0200

    [feature/test_api] added simple Category entity

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::get('/categories', 'App\Http\Controllers\Api\CategoryController@index');
+// Route::get('/category/{id}', 'App\Http\Controllers\Api\CategoryController@show');
+// Route::post('/category', 'App\Http\Controllers\Api\CategoryController@store');
+// Route::put('/category/{id}', 'App\Http\Controllers\Api\CategoryController@update');
+// Route::delete('/category/{id}', 'App\Http\Controllers\Api\CategoryController@destroy');
+Route::get('categories', [CategoryController::class, 'index']);
