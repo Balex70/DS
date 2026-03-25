2f8a1084abefb49f5efeb295e89bb00cceb99b8a
Bachynskyi Alex <balex70@yahoo.com>
2026-03-25 12:12:53 +0200

    [feature/admin_template] added shadcn + basic layout for admin area with sidebar

---
+++ frontend/components/admin/admin-sidebar.tsx
@@ -0,0 +1,62 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { Home, User2, WalletCards } from "lucide-react"
+
+export function AdminSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <h1>Admin</h1>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenuItem className="list-none">
+              <SidebarMenuButton asChild>
+                <a href="/home">
+                  <Home />
+                  <span>Home</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>            
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Settings</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenuItem className="list-none">
+              <SidebarMenuButton asChild>
+                <a href="/payments">
+                  <WalletCards />
+                  <span>Payments</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>            
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+        <SidebarMenuItem>
+            <SidebarMenuButton>
+            <User2 /> Username
+            </SidebarMenuButton>
+        </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+      {/* <SidebarRail /> */}
+    </Sidebar>
+  )
+}
