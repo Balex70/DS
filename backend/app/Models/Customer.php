be5e8226dd786d9e3520fb86156a5b274463b9d4
Bachynskyi Alex <balex70@yahoo.com>
2026-05-19 16:47:58 +0300

    [feature/customers] added basic Customer entity

---
+++ backend/app/Models/Customer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Customer extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\CustomerFactory> */
+    use HasFactory, HasApiTokens, Notifiable, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
