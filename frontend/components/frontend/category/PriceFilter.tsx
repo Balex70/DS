7ff359a35d58a59a153e6c71d215310ad2389fb1
Bachynskyi Alex <balex70@yahoo.com>
2026-06-25 17:48:13 +0300

    [feature/filter_products] finish with price filter

--- frontend/components/frontend/category/PriceFilter.tsx
+++ frontend/components/frontend/category/PriceFilter.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { PriceRenderer } from "@/components/custom/PriceRenderer";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Slider } from "@/components/ui/slider";
+import { useEffect, useState } from "react";
 
 type PriceFilterProps = {
     min: number;
@@ -19,11 +20,16 @@ export function PriceFilter({
     onChange,
     setPriceApplied
 }: PriceFilterProps) {
+    const toDisplay = (cents: number) => (cents / 100).toString();
+    const toCents = (value: number) => Math.round(value * 100);
+
+    const [localValue, setLocalValue] = useState<[number, number]>(value);
+
     return (
-        <div className="space-y-4">
+        <div className="space-y-4 pt-2 px-3">
             <div>
                 <h3 className="text-sm font-medium">
-                    Price
+                    Price (USD)
                 </h3>
             </div>
 
@@ -31,16 +37,74 @@ export function PriceFilter({
                 min={min}
                 max={max}
                 step={10}
-                value={value}
-                onValueChange={(v) => onChange(v as [number, number])}
+                value={localValue}
+                onValueChange={(v) => {
+                        // set localValue and real value, because slider don't need additional validation
+                        setLocalValue(v as [number, number])
+                        onChange(v as [number, number]);
+                    }
+                }
             />
+            <div className="flex gap-2">
+                <Input
+                    type="number"
+                    value={toDisplay(localValue[0])}
+                    onBlur={() => {
+                        // only on blur check for safe inputs (min/max)
+                        // to make UX better (type numbers without interrupting the user)
+                        const safeMin = Math.max(min, Math.min(max, localValue[0]));
+                        const safeMax = Math.max(safeMin, Math.min(max, localValue[1]));
 
-            <div className="flex items-center justify-between text-sm text-muted-foreground">
-                <span><PriceRenderer value={value[0]} /></span>
-                <span><PriceRenderer value={value[1]} /></span>
+                        const safe: [number, number] = [safeMin, safeMax];
+
+                        onChange(safe); // update value (not localValue)
+                        setLocalValue(safe)
+                    }}
+                    onChange={(e) => {
+                        // Change only localValue (price) to make it possible validate on blur
+                        const num = Number(e.target.value);
+                        if (isNaN(num)) return;
+
+                        setLocalValue([
+                            toCents(num),
+                            localValue[1],
+                        ]);
+                    }}
+                    className="text-sm"
+                />
+
+                <Input
+                    type="number"
+                    value={toDisplay(localValue[1])}
+                    onBlur={() => {
+                        // only on blur check for safe inputs (min/max)
+                        // to make UX better (type numbers without interrupting the user)
+                        const safeMin = Math.max(min, Math.min(max, localValue[0]));
+                        const safeMax = Math.max(safeMin, Math.min(max, localValue[1]));
+
+                        const safe: [number, number] = [safeMin, safeMax];
+
+                        onChange(safe); // update value (not localValue)
+                        setLocalValue(safe)
+                    }}
+                    onChange={(e) => {
+                        // Change only localValue (price) to make it possible validate on blur
+                        const num = Number(e.target.value);
+                        if (isNaN(num)) return;
+
+                        setLocalValue([
+                            localValue[0],
+                            toCents(num),
+                        ]);
+                    }}
+                    className="text-sm"
+                />
             </div>
             <Button
-                onClick={() => setPriceApplied()}
+                onClick={() => {
+                    // trigger request to backend with actual price data
+                    setPriceApplied()
+                }}
             >
                 Apply filters
             </Button>
