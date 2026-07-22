b0badfe5e385c6664528c72736ff5899e07b6611
Bachynskyi Alex <balex70@yahoo.com>
2026-07-22 16:25:18 +0300

    [feature/styling] fix styling for sheet close button in bottom-customer-menu

--- frontend/components/frontend/customer/bottom-customer-menu.tsx
+++ frontend/components/frontend/customer/bottom-customer-menu.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
-import { User } from "lucide-react";
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { User, X } from "lucide-react";
 
 export function BottomCustomerMenu() {
     return (
@@ -24,7 +24,20 @@ export function BottomCustomerMenu() {
 
             <SheetContent side="bottom" className="!h-dvh max-h-dvh w-full rounded-none">
                 <SheetHeader className="pb-0">
-                    <SheetTitle>Customer</SheetTitle>
+                    <div className="flex items-center justify-between">
+                        <SheetTitle className="inline-flex items-center">
+                            Customer
+                        </SheetTitle>
+
+                        <SheetClose asChild>
+                            <Button
+                                variant="ghost"
+                                className="h-8 w-8 p-0"
+                            >
+                                <X className="!h-6 !w-6" />
+                            </Button>
+                        </SheetClose>
+                    </div>
                 </SheetHeader>
                 
                 <Separator className="my-0" />
