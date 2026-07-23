5711084d257cb5bd4732b2049d7d5c9c57379efc
Bachynskyi Alex <balex70@yahoo.com>
2026-07-23 17:14:59 +0300

    [feature/styling] removed maincategories-section

--- frontend/app/[locale]/(frontend)/page.tsx
+++ frontend/app/[locale]/(frontend)/page.tsx
@@ -1,25 +1,11 @@
-import { MaincategoriesSection } from "@/components/frontend/category/maincategories-section";
 import { HeroCarousel } from "@/components/frontend/main/hero-carousel";
 import { CategoriesSection } from "@/components/frontend/main/categories-section";
 
 export default function HomePage() {
     return (
-        <div className="space-y-6">
+        <div className="space-y-6 mb-12">
             <HeroCarousel />
             <CategoriesSection />
-            <div>
-                <h1 className="text-3xl font-bold">
-                    Categories
-                </h1>
-
-                <p className="text-muted-foreground">
-                    Browse our product categories
-                </p>
-            </div>
-
-            <div>
-                <MaincategoriesSection />
-            </div>
         </div>
     );
 }
