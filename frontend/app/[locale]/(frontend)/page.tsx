092064d20cd3f80011a1525a02e43f2ce9fba616
Bachynskyi Alex <balex70@yahoo.com>
2026-07-24 16:32:09 +0300

    [feature/styling] added catalog button under hero carousel for small devices

--- frontend/app/[locale]/(frontend)/page.tsx
+++ frontend/app/[locale]/(frontend)/page.tsx
@@ -1,10 +1,12 @@
 import { HeroCarousel } from "@/components/frontend/main/hero-carousel";
 import { CategoriesSection } from "@/components/frontend/main/categories-section";
+import SmMegaMenu from "@/components/frontend/megamenu/sm-mega-menu";
 
 export default function HomePage() {
     return (
         <div className="space-y-6 mb-12">
             <HeroCarousel />
+            <SmMegaMenu />
             <CategoriesSection />
         </div>
     );
---
+++ frontend/components/frontend/megamenu/sm-mega-menu.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { LayoutGrid } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+import { MobileCatalog } from "./mobile-catalog";
+import { useState } from "react";
+import BottomSheetHeader from "../bottom-sheet-header";
+import { useTranslations } from 'next-intl'
+
+export default function SmMegaMenu() {
+    const [open, setOpen] = useState(false);
+    const t = useTranslations('frontend')
+
+    return (
+        <Sheet open={open} onOpenChange={setOpen}>
+            <div className="flex justify-center md:hidden">
+                <SheetTrigger asChild>
+                    <Button
+                        variant="outline"
+                        className="w-full gap-2 mb-0"
+                    >
+                        <LayoutGrid className="h-4 w-4" />
+                        {t('header.catalog')}
+                    </Button>
+                </SheetTrigger>
+            </div>
+
+            <SheetContent side="bottom" className="!h-dvh max-h-dvh w-full rounded-none">
+                <BottomSheetHeader>{t('header.catalog')}</BottomSheetHeader>
+                
+                <Separator className="my-0" />
+
+                <div className="flex-1 min-h-0">
+                    <MobileCatalog onClose={() => setOpen(false)}/>
+                </div>
+            </SheetContent>
+        </Sheet>
+    );
+}
