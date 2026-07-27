9bcaf7ffcdbd100fdc1152fe471a20d1e4f12d94
Bachynskyi Alex <balex70@yahoo.com>
2026-07-27 16:48:35 +0300

    [feature/styling] moved country selection on checkout to separate component

--- frontend/components/frontend/checkout/ShippingForm.tsx
+++ frontend/components/frontend/checkout/ShippingForm.tsx
@@ -6,26 +6,9 @@ import { useShippingCalculate } from "@/hooks/use-shipping-calculate";
 import { ShippingMethod } from "@/types/shipping";
 import { CheckoutStatus, OrderPayload } from "@/types/order";
 import { ShippingMethodsSelector } from "./ShippingMethodsSelector";
-import {
-    Command,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
-} from "@/components/ui/command";
-
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover";
-import { Button } from "@/components/ui/button";
-import { Check, ChevronsUpDown } from "lucide-react";
-import { cn } from "@/lib/utils";
 import { FinalCarriers } from "./FinalCarriers";
-import { COUNTRIES, getSelectedCountry } from "@/config/countries";
 import { useCurrency } from "@/context/CurrencyContext";
+import { SelectCountryForm } from "./SelectCountryForm";
 
 type Props = {
     form: OrderPayload;
@@ -53,7 +36,6 @@ export function ShippingForm({
     const { currency } = useCurrency();
     const { data: shippingOptions = [], isLoading, isFetching } =  useShippingCalculate(payload, cartKey, currency);
     const getError = (field: string) => errors[field]?.[0];
-    const [openCountryPopover, setOpenCountryPopover] = useState(false);
 
     // trigger shipping calculation when address changes
     useEffect(() => {
@@ -93,8 +75,6 @@ export function ShippingForm({
         setForm(prev => ({ ...prev, [field]: value }));
         setCheckoutStatus("idle");
     }
-
-    const selectedCountry = getSelectedCountry(form.shipping_country);
 
     return (
         <div>
@@ -219,66 +199,12 @@ export function ShippingForm({
                     </div>
 
                     <div className="space-y-1">
-                        <Popover open={openCountryPopover} onOpenChange={setOpenCountryPopover}>
-                            <PopoverTrigger asChild>
-                                <Button
-                                    variant="outline"
-                                    role="combobox"
-                                    className="w-full justify-between"
-                                >
-                                    {selectedCountry?.name ?? "Select country"}
-
-                                    <ChevronsUpDown className="ml-2 h-4 w-4 opacity-50" />
-                                </Button>
-                            </PopoverTrigger>
-
-                            <PopoverContent align="start"
-                                sideOffset={4}
-                                className="w-[--radix-popover-trigger-width] p-0">
-                                <div className="w-[var(--radix-popover-trigger-width)]">
-                                    <Command>
-                                        <CommandInput placeholder="Search country..." />
-
-                                        <CommandList>
-                                            <CommandEmpty>No country found.</CommandEmpty>
-
-                                            <CommandGroup>
-                                                {COUNTRIES.map((country) =>
-                                                    country.code === "__divider__" ? (
-                                                        <div key="divider" className="my-2 border-t" />
-                                                    ) : (
-                                                        <CommandItem
-                                                            key={country.code}
-                                                            value={country.name}
-                                                            onSelect={() => {
-                                                                setShippingMethod(undefined);
-                                                                setPayload(null);
-                                                                setForm(prev => ({
-                                                                    ...prev,
-                                                                    shipping_country: country.code,
-                                                                }));
-                                                                setOpenCountryPopover(false);
-                                                            }}
-                                                        >
-                                                            <Check
-                                                                className={cn(
-                                                                    "mr-2 h-4 w-4",
-                                                                    form.shipping_country === country.code
-                                                                        ? "opacity-100"
-                                                                        : "opacity-0"
-                                                                )}
-                                                            />
-
-                                                            {country.name}
-                                                        </CommandItem>
-                                                    )
-                                                )}
-                                            </CommandGroup>
-                                        </CommandList>
-                                    </Command>
-                                </div>
-                            </PopoverContent>
-                        </Popover>
+                        <SelectCountryForm
+                            form={form}
+                            setForm={setForm}
+                            setShippingMethod={setShippingMethod}
+                            setPayload={setPayload}
+                        />
                         {getError("shipping_country") && (
                             <p className="text-sm text-red-500 mt-1">
                                 {getError("shipping_country")}
