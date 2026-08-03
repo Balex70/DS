dc41b81c6111c62e2a4b347bdef70d18eae6ad7d
Bachynskyi Alex <balex70@yahoo.com>
2026-08-03 17:51:39 +0300

    [feature/styling] added returns and refund page with translations

---
+++ frontend/app/[locale]/(frontend)/(store)/(company)/returns/page.tsx
@@ -0,0 +1,9 @@
+import { ReturnsComponent } from "@/components/frontend/company/returns-component";
+
+export default async function AboutPage() {
+    return (
+        <div className="container mx-auto px-0 sm:px-4 py-0">
+            <ReturnsComponent />
+        </div>
+    );
+}
---
+++ frontend/components/frontend/company/returns-component.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useTranslations } from 'next-intl'
+import { CreditCard, Headset, PackageX, RotateCcw } from "lucide-react";
+
+export function ReturnsComponent() {
+    const t = useTranslations('frontend')
+
+    return (
+        <div className="container py-4 lg:py-10">
+            <div className="mb-10 text-center">
+                <h1 className="text-4xl font-bold">
+                    {t('footer.returns.header')}
+                </h1>
+
+                <p className="mt-4 text-muted-foreground">
+                    {t('footer.returns.description')}
+                </p>
+            </div>
+
+            <div className="grid gap-6 md:grid-cols-2">
+                <Card>
+                    <CardHeader>
+                        <RotateCcw className="mb-2 h-8 w-8 text-primary" />
+                        <CardTitle>{t('footer.returns.return_eligibility')}</CardTitle>
+                    </CardHeader>
+
+                    <CardContent className="text-muted-foreground">
+                        {t('footer.returns.return_eligibility_description')}
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardHeader>
+                        <CreditCard className="mb-2 h-8 w-8 text-primary" />
+                        <CardTitle>{t('footer.returns.refund_process')}</CardTitle>
+                    </CardHeader>
+
+                    <CardContent className="text-muted-foreground">
+                        {t('footer.returns.refund_process_description')}
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardHeader>
+                        <PackageX className="mb-2 h-8 w-8 text-primary" />
+                        <CardTitle>{t('footer.returns.return_damage')}</CardTitle>
+                    </CardHeader>
+
+                    <CardContent className="text-muted-foreground">
+                        {t('footer.returns.return_damage_description')}
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardHeader>
+                        <Headset className="mb-2 h-8 w-8 text-primary" />
+                        <CardTitle>{t('footer.returns.return_help')}</CardTitle>
+                    </CardHeader>
+
+                    <CardContent className="text-muted-foreground">
+                        {t('footer.returns.return_help_description')}
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    );
+}
