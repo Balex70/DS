655ad61e51537862eb7e2781d1189fd4d57ed271
Bachynskyi Alex <balex70@yahoo.com>
2026-08-03 17:06:13 +0300

    [feature/styling] added basic FAQ page with translations

---
+++ frontend/app/[locale]/(frontend)/(store)/(company)/faq/page.tsx
@@ -0,0 +1,9 @@
+import { FAQComponent } from "@/components/frontend/company/faq-component";
+
+export default async function AboutPage() {
+    return (
+        <div className="container mx-auto px-0 sm:px-4 py-0">
+            <FAQComponent />
+        </div>
+    );
+}
