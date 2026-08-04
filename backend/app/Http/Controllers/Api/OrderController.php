37a88c79cfbd6d29281f031646556721d41810d6
Bachynskyi Alex <balex70@yahoo.com>
2026-08-04 18:08:39 +0300

    [feature/emails] added basic client order processing and shipped mails

--- backend/app/Http/Controllers/Api/OrderController.php
+++ backend/app/Http/Controllers/Api/OrderController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enums\OrderDsStatusEnum;
 use App\Enums\OrderStatusEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateOrderRequest;
 use App\Http\Resources\OrderResource;
+use App\Mail\Client\ClientOrderProcessing;
+use App\Mail\Client\ClientOrderShipped;
 use App\Models\Order;
 use App\Services\OrderService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class OrderController extends Controller
 {
@@ -73,6 +77,8 @@ class OrderController extends Controller
         $order->load('items');
 
         $this->service->sendOrder($order);
+
+        Mail::to($order->shipping_email)->queue(new ClientOrderProcessing($order));
     }
 
     public function checkOrderStatusInDSProvider(Order $order)
@@ -90,6 +96,12 @@ class OrderController extends Controller
         $order->status = $this->service->toOrderStatus($responseData['data']['orderStatus']);
         $order->ds_status = $this->service->toDsStatus($responseData['data']['orderStatus']);
         $order->save();
+
+        // OrderDsStatusEnum::SHIPPED -> means in transit to the client
+        // Need to send email client and admin about that
+        if ($this->service->toDsStatus($responseData['data']['orderStatus']) === OrderDsStatusEnum::SHIPPED) {
+            Mail::to($order->shipping_email)->queue(new ClientOrderShipped($order));
+        }
 
         return response()->json([
             'success' => true,
---
+++ backend/app/Mail/Client/ClientOrderProcessing.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail\Client;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ClientOrderProcessing extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(private Order $order)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Order Processing',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mails.client.order-processing',
+            with: [
+                'orderNumber' => $this->order->order_number,
+                'fullName' => $this->order->shipping_full_name,
+                'link' => '/orders',
+                'message' => "Some message"
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
