64cc66898b88ccf5ced19c0d6738e879eebf11c8
Bachynskyi Alex <balex70@yahoo.com>
2026-08-13 19:11:46 +0300

    [feature/static_translations] checkout: payment-result

--- frontend/components/frontend/checkout/payment-result-component.tsx
+++ frontend/components/frontend/checkout/payment-result-component.tsx
@@ -4,31 +4,31 @@ import { Button } from "@/components/ui/button";
 import { useOrderByPublicToken } from "@/hooks/use-order-by-public-token";
 import { CheckCircle2 } from "lucide-react";
 import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 
 export function CheckoutPaymentResultComponent({ token }: { token: string | undefined }) {
     const { data: order, isLoading } = useOrderByPublicToken(token);
+    const t = useTranslations('frontend')
     return (
         <div className="container mx-auto max-w-2xl px-0 sm:px-4 py-2 sm:py-14">
             <div className="rounded-lg border bg-card p-4 sm:p-6 text-center shadow-sm">
                 <CheckCircle2 className="mx-auto mb-4 sm:mb-6 h-12 sm:h-16 w-12 sm:w-16 text-green-600" />
 
                 <h1 className="mb-4 text-xl sm:text-3xl font-bold">
-                    Thank you for your order!
+                    {t('checkout.payment_result.header')}
                 </h1>
 
                 <p className="mb-4 text-muted-foreground">
-                    We have received your payment attempt and are currently
-                    processing it.
+                    {t('checkout.payment_result.text_1')}
                 </p>
 
                 <p className="mb-8 text-muted-foreground">
-                    Your order status will be updated automatically once the
-                    payment provider confirms the transaction.
+                    {t('checkout.payment_result.text_2')}
                 </p>
 
                 {!isLoading && order && (
                     <div className="rounded-md bg-muted px-4 py-3 text-sm">
-                        Order number:{" "}
+                        {t('checkout.payment_result.order_number')}:{" "}
                         <span className="font-semibold">
                             #{order.order_number}
                         </span>
@@ -37,7 +37,7 @@ export function CheckoutPaymentResultComponent({ token }: { token: string | unde
                 
                 <Button asChild className="mt-3">
                     <Link href="/">
-                        Continue shopping
+                        {t('checkout.payment_result.continue_button')}
                     </Link>
                 </Button>
                 
