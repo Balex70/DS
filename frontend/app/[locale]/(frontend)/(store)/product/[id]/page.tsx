00dee20ea015a8b4e72791fbbeddf69393caf7f9
Bachynskyi Alex <balex70@yahoo.com>
2026-09-02 16:32:34 +0300

    [feature/ci_cd] fix product page params type

--- frontend/app/[locale]/(frontend)/(store)/product/[id]/page.tsx
+++ frontend/app/[locale]/(frontend)/(store)/product/[id]/page.tsx
@@ -3,7 +3,9 @@ import { ProductDetail } from "@/components/frontend/product/product-detail";
 export default async function ProductPage({
     params,
 }: {
-    params: { id: number };
+    params: Promise<{
+        id: string;
+    }>;
 }) {
     const { id } = await params;
     return (
