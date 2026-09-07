3cdf350f66ffda69f0b12a93b3e54ecead0547e8
Bachynskyi Alex <balex70@yahoo.com>
2026-09-07 17:32:17 +0300

    [feature/emails_improve] improve admin order created email

--- backend/resources/views/mails/admin/order-created.blade.php
+++ backend/resources/views/mails/admin/order-created.blade.php
@@ -13,18 +13,4 @@
     Order Number: <strong>{{ $orderNumber }}</strong>.
 </p>
 
-<p style="margin:32px 0;">
-    <a
-        href="{{ $link }}"
-        style="display:inline-block;background:#0f6776;color:#ffffff;text-decoration:none;padding:12px 24px;border-radius:6px;"
-    >
-        View Order
-    </a>
-</p>
-
-<p>
-    Thanks,<br>
-    {{ config('app.name') }}
-</p>
-
 @endsection
