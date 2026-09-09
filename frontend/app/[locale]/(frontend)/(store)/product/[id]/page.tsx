35c5417194234bb2e78506da6d48b1f45c463e47
Bachynskyi Alex <balex70@yahoo.com>
2026-09-09 16:31:51 +0300

    [feature/seo] added generateMetadata for product page

--- frontend/app/[locale]/(frontend)/(store)/product/[id]/page.tsx
+++ frontend/app/[locale]/(frontend)/(store)/product/[id]/page.tsx
@@ -1,4 +1,39 @@
+import { getProduct } from "@/actions/productActions";
 import { ProductDetail } from "@/components/frontend/product/product-detail";
+import { Metadata } from "next";
+
+type Props = {
+    params: Promise<{
+        id: string;
+        locale: string;
+    }>;
+};
+
+export async function generateMetadata({
+    params,
+}: Props): Promise<Metadata> {
+    const { id, locale } = await params;
+
+    try {
+        const product = await getProduct(id);
+
+        const translation = product.translations?.find(
+            (item) => item.locale === locale
+        );
+
+        const title = translation?.name ?? product.name_processed ?? product.name_raw;
+        const description = translation?.description ?? product.description_processed ?? product.description_raw ?? "";
+
+        return {
+            title,
+            description,
+        };
+    } catch {
+        return {
+            title: "Product",
+        };
+    }
+}
 
 export default async function ProductPage({
     params,
