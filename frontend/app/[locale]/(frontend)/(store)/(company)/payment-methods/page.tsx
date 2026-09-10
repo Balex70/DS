746a1d1a7703d3ea5f85e7bf23398cf00c3261b1
Bachynskyi Alex <balex70@yahoo.com>
2026-09-10 17:56:43 +0300

    [feature/seo] seo for payment methods page

--- frontend/app/[locale]/(frontend)/(store)/(company)/payment-methods/page.tsx
+++ frontend/app/[locale]/(frontend)/(store)/(company)/payment-methods/page.tsx
@@ -1,5 +1,19 @@
+import { getSettings } from "@/actions/settingsActions";
 import { PaymentMethodsComponent } from "@/components/frontend/company/payment-methods-component";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 
+export async function generateMetadata(): Promise<Metadata> {
+    const settings = await getSettings();
+    const t = await getTranslations('frontend');
+
+    const storeName = (settings?.["store.name"] && settings?.["store.name"] !== '') ? ' | ' + settings?.["store.name"] : '';
+
+    return {
+        title: t('footer.payment_methods_section.meta_title') + storeName,
+        description: t('footer.payment_methods_section.meta_description'),
+    };
+}
 
 export default async function PaymentMethodsPage() {
     return (
