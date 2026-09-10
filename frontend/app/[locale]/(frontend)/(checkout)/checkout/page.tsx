37c5e857ffd57e0354e9ba91f2fca185cda0f554
Bachynskyi Alex <balex70@yahoo.com>
2026-09-10 18:53:04 +0300

    [feature/seo] seo for checkout page

--- frontend/app/[locale]/(frontend)/(checkout)/checkout/page.tsx
+++ frontend/app/[locale]/(frontend)/(checkout)/checkout/page.tsx
@@ -1,4 +1,19 @@
+import { getSettings } from "@/actions/settingsActions";
 import { CheckoutComponent } from "@/components/frontend/checkout/checkout-component";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata(): Promise<Metadata> {
+    const settings = await getSettings();
+    const t = await getTranslations('frontend');
+
+    const storeName = (settings?.["store.name"] && settings?.["store.name"] !== '') ? ' | ' + settings?.["store.name"] : '';
+
+    return {
+        title: t('checkout.meta_title') + storeName,
+        description: t('checkout.meta_description'),
+    };
+}
 
 export default async function CheckoutPage() {
     return (
