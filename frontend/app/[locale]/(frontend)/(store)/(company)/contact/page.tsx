bf476bc39eac9c58fe819da9f57f1265aa651c52
Bachynskyi Alex <balex70@yahoo.com>
2026-09-10 17:41:05 +0300

    [feature/seo] seo for contact us page

--- frontend/app/[locale]/(frontend)/(store)/(company)/contact/page.tsx
+++ frontend/app/[locale]/(frontend)/(store)/(company)/contact/page.tsx
@@ -1,10 +1,26 @@
+import { getSettings } from "@/actions/settingsActions";
 import { ContactComponent } from "@/components/frontend/company/contact-component";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 
+export async function generateMetadata(): Promise<Metadata> {
+    const settings = await getSettings();
+    const t = await getTranslations('frontend');
+
+    const storeName = (settings?.["store.name"] && settings?.["store.name"] !== '') ? ' | ' + settings?.["store.name"] : '';
+
+    return {
+        title: t('footer.contact_us_section.meta_title') + storeName,
+        description: t('footer.contact_us_section.meta_description'),
+    };
+}
 
 export default async function ContactPage() {
+    const settings = await getSettings();
+
     return (
         <div className="container mx-auto px-0 sm:px-4 py-0">
-            <ContactComponent />
+            <ContactComponent settings={settings} />
         </div>
     );
 }
