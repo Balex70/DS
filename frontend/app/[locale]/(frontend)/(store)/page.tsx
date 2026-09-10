7ede0368c16eb0e1efd35a86ac02414efc746b00
Bachynskyi Alex <balex70@yahoo.com>
2026-09-10 18:49:36 +0300

    [feature/seo] seo for main/home page

--- frontend/app/[locale]/(frontend)/(store)/page.tsx
+++ frontend/app/[locale]/(frontend)/(store)/page.tsx
@@ -1,6 +1,21 @@
 import { HeroCarousel } from "@/components/frontend/main/hero-carousel";
 import { CategoriesSection } from "@/components/frontend/main/categories-section";
 import SmMegaMenu from "@/components/frontend/megamenu/sm-mega-menu";
+import { getSettings } from "@/actions/settingsActions";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata(): Promise<Metadata> {
+    const settings = await getSettings();
+    const t = await getTranslations('frontend');
+
+    const storeName = (settings?.["store.name"] && settings?.["store.name"] !== '') ? ' | ' + settings?.["store.name"] : '';
+
+    return {
+        title: t('main.meta_title') + storeName,
+        description: t('main.meta_description'),
+    };
+}
 
 export default function HomePage() {
     return (
