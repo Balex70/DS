8a55cab19eddeb9e0141fbe831800766c01a2200
Bachynskyi Alex <balex70@yahoo.com>
2026-09-10 19:19:28 +0300

    [feature/seo] added noindex and nofollow for search page

--- frontend/app/[locale]/(frontend)/(store)/search/layout.tsx
+++ frontend/app/[locale]/(frontend)/(store)/search/layout.tsx
@@ -1,4 +1,12 @@
 import { Sidebar } from "@/components/frontend/sidebar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    robots: {
+        index: false,
+        follow: false,
+    },
+};
 
 export default function Layout({
     children,
