976cc0afc766674bd1835fb183bdb7fe9ca082f5
Bachynskyi Alex <balex70@yahoo.com>
2026-09-14 16:24:16 +0300

    [feature/images_fixes] removed ia_url from admin area

--- frontend/components/admin/products/ProductImagesDrawer.tsx
+++ frontend/components/admin/products/ProductImagesDrawer.tsx
@@ -26,21 +26,13 @@ export function ProductImagesDrawer({
                     Product image {!product.last_enrichment_at && <span>(image from CJ)</span>}
                   </Label>
                   <div className="aspect-square overflow-hidden rounded-lg border bg-muted">
-                    {product.big_image?.ai_url ? (
-                      <NextImageWithReplace
-                        src={product.big_image.ai_url}
-                        alt={product.name_raw}
-                        width={200}
-                        height={200}
-                        imageClassName="h-full w-full object-cover"
-                      />
-                    ) : (
-                      <img
-                        src={product.big_image.original_url}
-                        alt={product.name_raw}
-                        className="h-full w-full object-cover"
-                      />
-                    )}
+                    <NextImageWithReplace
+                      src={product.big_image.original_url}
+                      alt={product.name_raw}
+                      width={200}
+                      height={200}
+                      imageClassName="h-full w-full object-cover"
+                    />
                   </div>
                 </>
               )}
@@ -49,67 +41,30 @@ export function ProductImagesDrawer({
             {/* RIGHT: TABS */}
             {product.images !== undefined && product.images?.length > 0 && (
               <div className="w-full max-w-52">
-                <Tabs defaultValue="original">
-
-                  <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="original">Other</TabsTrigger>
-                    <TabsTrigger value="ai">AI</TabsTrigger>
-                  </TabsList>
-
-                  {/* ORIGINAL IMAGES */}
-                  <TabsContent value="original">
-                    <Carousel className="w-full sm:max-w-xs mt-2">
-                      <CarouselContent>
-                        {product.images !== undefined && product.images.map((img, idx) => (
-                          <CarouselItem key={idx}>
-                            <div className="p-1">
-                              <Card>
-                                <CardContent className="flex aspect-square items-center justify-center p-2">
-                                  <NextImageWithReplace
-                                    src={img.original_url}
-                                    alt={idx.toString()}
-                                    width={200}
-                                    height={200}
-                                    imageClassName="h-full w-full object-cover"
-                                  />
-                                </CardContent>
-                              </Card>
-                            </div>
-                          </CarouselItem>
-                        ))}
-                      </CarouselContent>
-                      <CarouselPrevious />
-                      <CarouselNext />
-                    </Carousel>
-                  </TabsContent>
-
-                  {/* AI IMAGES */}
-                  <TabsContent value="ai">
-                    <Carousel className="w-full sm:max-w-xs mt-2">
-                      <CarouselContent>
-                        {product.images !== undefined && product.images.filter(img => img.ai_url).map((img, idx) => (
-                          <CarouselItem key={idx}>
-                            <div className="p-1">
-                              <Card>
-                                <CardContent className="flex aspect-square items-center justify-center p-2">
-                                  <NextImageWithReplace
-                                    src={img.ai_url}
-                                    alt={idx.toString()}
-                                    width={200}
-                                    height={200}
-                                    imageClassName="h-full w-full object-cover"
-                                  />
-                                </CardContent>
-                              </Card>
-                            </div>
-                          </CarouselItem>
-                        ))}
-                      </CarouselContent>
-                      <CarouselPrevious />
-                      <CarouselNext />
-                    </Carousel>
-                  </TabsContent>
-                </Tabs>
+                <Label className="mb-2">Other images</Label>
+                <Carousel className="w-full sm:max-w-xs mt-2">
+                  <CarouselContent>
+                    {product.images !== undefined && product.images.map((img, idx) => (
+                      <CarouselItem key={idx}>
+                        <div className="p-1">
+                          <Card>
+                            <CardContent className="flex aspect-square items-center justify-center p-2">
+                              <NextImageWithReplace
+                                src={img.original_url}
+                                alt={idx.toString()}
+                                width={200}
+                                height={200}
+                                imageClassName="h-full w-full object-cover"
+                              />
+                            </CardContent>
+                          </Card>
+                        </div>
+                      </CarouselItem>
+                    ))}
+                  </CarouselContent>
+                  <CarouselPrevious />
+                  <CarouselNext />
+                </Carousel>
               </div>
             )}
           </div>
