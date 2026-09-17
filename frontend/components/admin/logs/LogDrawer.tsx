b4ccdaa468a2b59ec2db6dfa4b8ab68e58776f53
Bachynskyi Alex <balex70@yahoo.com>
2026-09-17 18:04:23 +0300

    [feature/logs] small adjustment in LogDrawer

--- frontend/components/admin/logs/LogDrawer.tsx
+++ frontend/components/admin/logs/LogDrawer.tsx
@@ -10,6 +10,42 @@ import { CardContent } from "@/components/ui/card"
 import { Log } from "@/types/log"
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
 import { format } from "date-fns"
+import { Badge } from "@/components/ui/badge"
+
+const LevelBadge = ({ level }: { level: string }) => {
+    switch (level) {
+        case "info":
+          return (
+            <Badge className="bg-blue-200 text-blue-800 hover:bg-blue-100">
+              INFO
+            </Badge>
+          )
+        case "success":
+          return (
+            <Badge className="bg-green-200 text-green-800 hover:bg-green-100">
+              SUCCESS
+            </Badge>
+          )
+        case "warning":
+          return (
+            <Badge className="bg-yellow-200 text-yellow-800 hover:bg-yellow-100">
+              WARNING
+            </Badge>
+          )
+        case "error":
+          return (
+            <Badge className="bg-red-200 text-red-800 hover:bg-red-100">
+              ERROR
+            </Badge>
+        )
+        default:
+          return (
+            <Badge className="bg-gray-200 text-gray-800 hover:bg-gray-100">
+              UNKNOWN
+            </Badge>
+          )
+    }
+}
 
 export function LogDrawer({
   open,
@@ -31,7 +67,7 @@ export function LogDrawer({
             <CardContent className="space-y-4">
                 <FieldGroup className="grid grid-cols-2">
                     <Field>
-                        <FieldLabel>Description Processed</FieldLabel>
+                        <FieldLabel>ID</FieldLabel>
                         <span className="text-md text-muted-foreground">
                             {log.id}
                         </span>
@@ -39,11 +75,11 @@ export function LogDrawer({
                     <Field>
                         <FieldLabel>Level</FieldLabel>
                         <div className="text-md text-muted-foreground">
-                            {log.level}
+                            <LevelBadge level={log.level} />
                         </div>
                     </Field>
                     <Field>
-                        <FieldLabel>Price</FieldLabel>
+                        <FieldLabel>Realm</FieldLabel>
                         <div className="text-md text-muted-foreground">
                             {log.realm}
                         </div>
@@ -55,7 +91,7 @@ export function LogDrawer({
                         </div>
                     </Field>
                     <Field>
-                        <FieldLabel>Suggested Price</FieldLabel>
+                        <FieldLabel>Created At</FieldLabel>
                         <div className="text-md text-muted-foreground">
                             {log.created_at ? format(new Date(log.created_at), "PPpp") : "No"}
                         </div>
