0e604ed4bdb7581b49c65bc23bc54dcade7c075c
Bachynskyi Alex <balex70@yahoo.com>
2026-09-17 15:45:23 +0300

    [feature/logs] started with app logs

---
+++ backend/app/Enums/AppLogRealmEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum AppLogRealmEnum: string
+{
+    case CATEGORY = 'category';
+    case PRODUCT = 'product';
+    case ORDER = 'order';
+    case SYSTEM = 'system';
+    case AUTH = 'auth';
+}
