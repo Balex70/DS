db7fa104f7745b46e4a0f4f6056aee7178c77764
Bachynskyi Alex <balex70@yahoo.com>
2026-09-18 16:53:36 +0300

    [feature/logs] added applog for moving order from draft to created (LockDraftOrder)

--- backend/app/Listeners/LockDraftOrder.php
+++ backend/app/Listeners/LockDraftOrder.php
@@ -2,6 +2,9 @@
 
 namespace App\Listeners;
 
+use App\Actions\CreateAppLogAction;
+use App\Enums\AppLogLevelEnum;
+use App\Enums\AppLogRealmEnum;
 use App\Enums\OrderStatusEnum;
 use App\Enums\PaymentStatusEnum;
 use App\Events\PaymentInitiated;
@@ -11,7 +14,7 @@ class LockDraftOrder
     /**
      * Create the event listener.
      */
-    public function __construct()
+    public function __construct(private CreateAppLogAction $createAppLogAction)
     {
         //
     }
@@ -22,13 +25,27 @@ class LockDraftOrder
     public function handle(PaymentInitiated $event): void
     {
         $order = $event->order->fresh();
+        try {
+            // Lock the order by changing it from DRAFT to CREATED
+            if ($order->status === OrderStatusEnum::DRAFT) {
+                $order->update([
+                    'status' => OrderStatusEnum::CREATED,
+                    'payment_status' => PaymentStatusEnum::PENDING
+                ]);
 
-        // Lock the order by changing it from DRAFT to CREATED
-        if ($order->status === OrderStatusEnum::DRAFT) {
-            $order->update([
-                'status' => OrderStatusEnum::CREATED,
-                'payment_status' => PaymentStatusEnum::PENDING
-            ]);
+                $this->createAppLogAction->execute(
+                    AppLogLevelEnum::SUCCESS,
+                    AppLogRealmEnum::ORDER,
+                    "Order (ID: {$order->id}) moved from DRAFT to CREATED, payment status changed to PENDING"
+                );
+            }
+        } catch (\Throwable $e) {
+            $this->createAppLogAction->execute(
+                AppLogLevelEnum::ERROR,
+                AppLogRealmEnum::ORDER,
+                "LockDraftOrder: Failed to move order [{$order->id}] from DRAFT to CREATED: {$e->getMessage()}"
+            );
+            throw $e;
         }
     }
 }
